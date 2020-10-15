60c3c1f19e5c10cedca89a12f7ec746f275bbcbf
nts2pd <nts2pd@gmail.com>
2020-10-14 20:33:00 -0400

    create custom api methods, use auth token, register new user, add restrictions

--- api/models.py
+++ api/models.py
@@ -7,6 +7,21 @@ class Movie(models.Model):
     title = models.CharField(max_length=35)
     description = models.TextField()
 
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+
+    def avg_rating(self):
+        sum = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            sum += rating.stars
+
+        if len(ratings) > 0:
+            return sum / len(ratings)
+        else:
+            return 0
+
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, blank=True, null=True, on_delete=models.CASCADE)
